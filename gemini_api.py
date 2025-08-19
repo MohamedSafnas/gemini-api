b208fc85c6b995ed7e1d62da89143e616e5b845b
Mohamed_Safnas <s23010675@ousl.lk>
2025-08-19 13:53:12 +0530

    Update gemini_api.py

--- gemini_api.py
+++ gemini_api.py
@@ -24,7 +24,7 @@ def generate_steps():
         print("🎯 Goal:", goal)
 
     
-        model = genai.GenerativeModel("gemini-1.5-pro")
+        model = genai.GenerativeModel("gemini-1.5-flash")
 
         response = model.generate_content(f"Suggest 5 step-by-step instructions to achieve this goal: {goal}")
 
